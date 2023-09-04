d81b8e7afc18755ac2b57d337b2c0d59fd1a48a1
Albert <superalbertem@gmail.com>
2023-09-04 10:50:07 +0200

    Get rid of useless tabs

--- src/widgets/Sidebar/lib/tabs.ts
+++ src/widgets/Sidebar/lib/tabs.ts
@@ -1,40 +1,31 @@
 import { icons } from '../../../shared/Icons';
 import { TabInfo } from '../types';
 
-const { house, grid, analytics, market, swap, speed, star, strategies } = icons;
+const { house, market, swap, speed, star } = icons;
 
 export enum Tabs {
   DISCOVER = 'Discover',
   DASHBOARD = 'Dashboard',
-  PROTOCOLS = 'Protocols',
-  STRATEGIES = 'Strategies',
   REWARDS = 'Rewards',
   DEBT = 'Debt',
   SWAP = 'Swap',
-  ANALYTICS = 'Analytics',
   MARKET = 'Market',
 }
 
 export enum LinksTo {
   DISCOVER = '/',
   DASHBOARD = '/dashboard',
-  PROTOCOLS = '/protocols',
-  STRATEGIES = '/strategies',
   REWARDS = '/rewards',
   DEBT = '/debts',
   SWAP = '/swap',
-  ANALYTICS = '/analytics',
   MARKET = '/market',
 }
 
 export const tabs: TabInfo[] = [
   { name: Tabs.DISCOVER, icon: star, to: LinksTo.DISCOVER },
   { name: Tabs.DASHBOARD, icon: house, to: LinksTo.DASHBOARD },
-  { name: Tabs.PROTOCOLS, icon: grid, to: LinksTo.PROTOCOLS },
-  { name: Tabs.STRATEGIES, icon: strategies, to: LinksTo.STRATEGIES },
   { name: Tabs.REWARDS, icon: star, to: LinksTo.REWARDS },
   { name: Tabs.DEBT, icon: speed, to: LinksTo.DEBT },
   { name: Tabs.SWAP, icon: swap, to: LinksTo.SWAP },
-  { name: Tabs.ANALYTICS, icon: analytics, to: LinksTo.ANALYTICS },
   { name: Tabs.MARKET, icon: market, to: LinksTo.MARKET },
 ];
