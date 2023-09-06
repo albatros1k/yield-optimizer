06e54cb60f594d76a61211735648b5865b46b5b5
Albert <superalbertem@gmail.com>
2023-09-06 15:46:36 +0200

    deploy script is updated

--- src/features/pagenotfound/PageNotFound.tsx
+++ src/features/pagenotfound/PageNotFound.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import { styles } from './styles';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { makeStyles } from '@material-ui/styles';
 import image from '../../images/404image.svg';
 import { Button } from '../../components/Button';
@@ -12,11 +12,11 @@ const useStyles = makeStyles(styles);
 export const PageNotFound = () => {
   const classes = useStyles();
   const { t } = useTranslation();
-  const history = useHistory();
+  const navigate = useNavigate();
 
   const handleOpen = useCallback(() => {
-    history.push(`/`);
-  }, [history]);
+    navigate(`/`);
+  }, [navigate]);
 
   return (
     <Container maxWidth="lg">
