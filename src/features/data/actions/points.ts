d29725688ad512c15517a1640ee8963ca8905af0
Albert <superalbertem@gmail.com>
2023-12-08 15:43:00 +0100

    New Nft contract

--- src/features/data/actions/points.ts
+++ src/features/data/actions/points.ts
@@ -19,18 +19,28 @@ export const fetchGalaxyPoints = createAsyncThunk(
 // user with nft - '0xBcf23e91E99D5c0DC16F2C9eF99036362C0ab81e'
 export const getNftData = createAsyncThunk('points/getNftData', async (_, thunkApi) => {
   try {
-    const contractAddress = '0x705c3dfb340fc9c75d30e1ea41299d34159f3638';
-    const tokenId = 1;
+    const contractAddress = '0x1992A7D5Fe9E6A81453151E18cAE7777b9D9452F';
     const walletAddress = (thunkApi.getState() as BeefyState).user.wallet.address;
     const provider = new ethers.JsonRpcProvider(
       `https://polygon-mainnet.infura.io/v3/${import.meta.env.VITE_APP_INFURA_KEY}`
     );
     const nftContract = new ethers.Contract(contractAddress, christmasAbi, provider);
+    const numMinted = await nftContract.getNumMinted();
+    let tokenId;
+
+    for (let i = 1; i <= parseInt(numMinted); i++) {
+      const isOwnerOf = await nftContract.isOwnerOf(walletAddress, i);
+
+      if (isOwnerOf) {
+        tokenId = i;
+      }
+    }
+
     const balance = await nftContract.balanceOf(walletAddress);
 
     const { data: metadata } = await axios(await nftContract.tokenURI(tokenId));
 
-    return balance > 0 ? metadata : null;
+    return balance > 0 && typeof tokenId !== 'undefined' ? metadata : null;
   } catch (error) {
     console.error('Error checking NFT balance:', error.message);
     return null;
